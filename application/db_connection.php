7eb18ba08234a4a0bf8fd59a8907b1d9dc2b7495
Francesco Bosso <fr.bosso@outlook.it>
2017-05-24 01:14:24 +0200

    Inserimento Query

--- application/db_connection.php
+++ application/db_connection.php
@@ -18,4 +18,69 @@ class Db {
         }
         return self::$instance;
     }
+
+    public static function init() {
+        $query = "
+SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";
+SET time_zone = \"+00:00\";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `animazione`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_friendlyname` varchar(100) NOT NULL,
+  `user_mail` varchar(100) NOT NULL,
+  `user_access` int(11) NOT NULL DEFAULT '1',
+  `user_password` char(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_friendlyname`, `user_mail`, `user_access`, `user_password`) VALUES
+(1, 'admin', 'Francesco Bosso', 'francesco@outlook.ita', 3, 'd033e22ae348aeb5660fc2140aec35850c4da997'),
+(2, 'test', 'Mario Rossi', 'rossimario@gmail.com', 1, '250e77f12a5ab6972a0895d290c4792f0a326ea8');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_mail` (`user_mail`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+        ";
+    }
 }
